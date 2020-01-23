a267f69098a03070c508ed4766702035ef7b4b5b
DaaMonkey <simonetomi@gmail.com>
2020-01-23 20:19:54 +0100

    Dialogue bug to fix

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public string[] dialogue;
+    public Text dialogueText;
+
+    private float timeLeft = 3;
+
+    public Animator dialogueAnim;
+
+    private bool isRandomized;
+
+    private void Update()
+    {
+        if (isRandomized)
+        {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                timeLeft = 3;
+            }
+        }
+    }
+
+    public void RandomDialog()
+    {
+        isRandomized = true;
+        dialogueText.text = dialogue[Random.Range(0, dialogue.Length)];
+        dialogueAnim.Play("Right_Popup");
+
+
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public Image bar;
-    //public Image limitLine;
     private float limitFill = 0.5f; // Limit of the fillBar before the camera move
 
     private float increaseValue = 0.3f; // Initial speed
@@ -16,23 +15,18 @@ public class GameManager : MonoBehaviour
 
     private float timeLeft = 3; // Increase difficulty every TOT seconds
     private float timeBeforeBadDialogue = 10;
-    //public bool badDialogue;
 
     private float cameraUpSpeed = 0.4f;
     private float cameraDownSpeed = 0.8f;
 
-    public Transform dialogue;
-
     public Text stressText;
 
-
+    private bool isRedZone;
 
     void Start()
     {
         stressText.text = "" + increaseValue.ToString("0.0");
         Vector3 mainCamera = Camera.main.transform.position;
-
-        //limitLine.fillAmount = limitFill / 1;
     }
 
     void Update()
@@ -44,69 +38,53 @@ public class GameManager : MonoBehaviour
             if (increaseValue <= maxIncreaseValue)
             {
                 increaseValue += increaseValueAmount;
+
+                //animazione!!
+                /*if(bar.fillAmount < limitFill)
+                {
+                    GetComponent<DialogueManager>().RandomDialog();
+                }*/
             }
             stressText.text = "" + increaseValue.ToString("0.0");
         }
 
         bar.fillAmount -= increaseValue * Time.deltaTime;
 
-        /*if (!badDialogue)
-        {
-            timeLeft -= Time.deltaTime;
-            if (timeLeft < 0)
-            {
-                timeLeft = 3;
-                if (increaseValue <= maxIncreaseValue)
-                {
-                    increaseValue += increaseValueAmount;
-                }
-                stressText.text = "" + increaseValue.ToString("0.0");
-            }
-
-            bar.fillAmount -= increaseValue * Time.deltaTime;
-        }*/
-
         if (bar.fillAmount < limitFill)
         {
-            if(Camera.main.transform.position.y >= -2f)
+            if (!isRedZone)
+            {
+                isRedZone = true;
+                GetComponent<DialogueManager>().RandomDialog();
+            }
+
+            if (Camera.main.transform.position.y >= -2f)
             {
                 bar.color = Color.red;
                 Camera.main.transform.position = new Vector3(0, Camera.main.transform.position.y - cameraUpSpeed * Time.deltaTime, -10);
-            }
-
-            if(dialogue.localScale.y >= 0.3f)
-            {
-                dialogue.localScale = new Vector2(dialogue.localScale.x - 0.2f * Time.deltaTime, dialogue.localScale.y - 0.2f * Time.deltaTime);
             }
 
             timeBeforeBadDialogue -= Time.deltaTime;
             if (timeBeforeBadDialogue < 0)
             {
                 timeBeforeBadDialogue = 10;
-                //badDialogue = true;
                 Debug.Log("Bad Dialogue"); // Blocca la barra, i comandi, porta su l'inquadratura e inizia il dialogo
             }
         }
         else if (bar.fillAmount > limitFill)
         {
-            if(Camera.main.transform.position.y <= 0f)
+            if (isRedZone)
+            {
+                isRedZone = false;
+            }
+
+            if (Camera.main.transform.position.y <= 0f)
             {
                 bar.color = Color.green;
                 Camera.main.transform.position = new Vector3(0, Camera.main.transform.position.y + cameraDownSpeed * Time.deltaTime, -10);
             }
 
-            if (dialogue.localScale.y <= 1)
-            {
-                dialogue.localScale = new Vector2(dialogue.localScale.x + 0.2f * Time.deltaTime, dialogue.localScale.y + 0.2f * Time.deltaTime);
-            }
-
             timeBeforeBadDialogue = 10;
         }
-
-        /*if (badDialogue)
-        {
-            if (Camera.main.transform.position.y <= 0f)
-                Camera.main.transform.position = new Vector3(0, Camera.main.transform.position.y + 1f * Time.deltaTime, -10);
-        }*/
     }
 }
